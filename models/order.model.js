dc0d411b066b4fa5ea824af67d4c6bcfafb6b450
CHIK SUM <chiksum.wong@gmail.com>
2019-04-20 17:10:06 +0800

    Update order.model.js

--- models/order.model.js
+++ models/order.model.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
 const OrderSchema = new mongoose.Schema({
-        products:[{product_id:String, quantity: Number, seller: String}],
+        products:[{product_id:String, product_name:String, quantity: Number, seller: String}],
         total_amount: { type: Number, required: true},
         buyer: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required:true},
+        addressee:{type:String, required:true},
         address: {type:String, required:true},
         phone_number:{type:String, required:true},
         payment_method:{type:Number, require:true},
